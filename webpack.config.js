d50de71fe6c0a353a13dc04d73731cc6336a052f
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-19 10:56:53 -0700

    add env file, new plugin and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -16,6 +17,7 @@ module.exports = {
     port: 9001
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'TITLE',
